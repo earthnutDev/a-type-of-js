04b48e42e79cb1ce769e02f46822c3e30345f241
lmssee <earthnut.dev@outlook.com>
2025-03-23 22:45:58 +0800

    小的更改

--- scripts/clean-package-json.js
+++ scripts/clean-package-json.js
@@ -1,11 +1,3 @@
-/** **  ** ** ** ** ** ** ** ** ** ****
- *  @Author earthnut
- *  @Email earthnut.dev@outlook.com
- *  @ProjectName a-type-of-js
- *  @FileName clean-package-json.js
- *  @CreateDate  周六  03/22/2025
- *  @Description 动态构建 package.json，移除不需要的 scripts、devDependencies 等
- **/
 import { pathJoin, readFileToJsonSync } from 'a-node-tools';
 import { writeFileSync } from 'node:fs';
 
@@ -13,7 +5,9 @@ const packageJson = readFileToJsonSync('./package.json');
 
 delete packageJson.scripts;
 delete packageJson.devDependencies;
+delete packageJson['lint-staged'];
 
+// eslint-disable-next-line no-undef
 const distPath = pathJoin(process.cwd(), './dist/package.json');
 
 writeFileSync(distPath, JSON.stringify(packageJson, null, 2));
