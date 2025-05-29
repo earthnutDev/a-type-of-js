d68b5dad64d81be68bed9c316afa359266986bdb
earth nut dev <earthnut.dev@outlook.com>
2025-05-29 12:05:15 +0800

    version: 1.0.1 2025-05-29 12:05:15 isType 第二参数作为回调函数时可使用一个参数，即要验证的数据本身

--- src/isType.test.ts
+++ src/isType.test.ts
@@ -1,7 +1,11 @@
+import { isUndefined } from 'a-type-of-js';
 import { isType } from './isType';
 import { testList } from './testData';
 
 describe('测试是否为某数据类型', () => {
+  describe('简单的代替 as', () => {
+    expect(isType<string>('a')).toBe(true);
+  });
   describe('以函数为媒', () => {
     test.each(testList)('测试值 %p 应当返回 %s', value => {
       expect(isType<typeof value>(value, () => true)).toBe(true);
@@ -16,5 +20,31 @@ describe('测试是否为某数据类型', () => {
     test.each(testList)('测试值 %p 应当返回 %s', value => {
       expect(isType<typeof value>(value)).toBe(true);
     });
+  });
+
+  describe('测试参数可用', () => {
+    const a: string | number = '10';
+
+    expect(isType<string>(a, input => typeof input === 'string')).toBe(true);
+
+    expect(isType<number>(a, input => typeof input === 'number')).toBe(true);
+
+    /**  cn  */
+    function cn(a: string | number) {
+      if (a as string) {
+        console.log('====================================');
+        console.log(a.toString(), () => a.toString() === a);
+        console.log('====================================');
+      }
+
+      if (isType<string>(a, a => !isUndefined(a.padEnd))) {
+        console.log('====================================');
+        console.log(a.charAt(1));
+        console.log('====================================');
+      }
+    }
+
+    cn(1);
+    cn('1');
   });
 });
--- src/isType.ts
+++ src/isType.ts
@@ -10,30 +10,7 @@
  *
  * 给出一个类型 `T` ，以返回值真假来判断 `input` 是否为 `T` 类型
  *
- * @param input 待检测的数据
- * @returns 返回 `true` 则说明该数据 `input` 类型为 `T` ，且在 Typescript 中进行类型收缩
- * @example
- *
- * ```ts
- * import  { isType } from 'a-type-of-js';
- *
- * console.log(isType<number>(1)); // true
- * console.log(isType<string>('1')); // true
- * ```
- */
-export function isType<T = string>(input: unknown): input is T;
-
-export function isType<T = string>(
-  input: unknown,
-  judgingConditions: boolean | (() => boolean),
-): input is T;
-
-/**
- *
- * 给出一个类型 `T` ，以返回值真假来判断 `input` 是否为 `T` 类型
- *
  * @param  input 待检测的数据
- * @param  judgingConditions Determine whether this type is used
  * @returns 返回 `true` 则说明该数据 `input` 类型为 `ReferenceError` ，且在 Typescript 中进行类型收缩
  * @example
  *
@@ -41,26 +18,87 @@ export function isType<T = string>(
  * import  { isType } from 'a-type-of-js';
  *
  * console.log(isType<number>(1)); // true
+ * console.log(isType<string>('1')); // true
+ * ```
+ *
+ */
+export function isType<T = string>(input: unknown): input is T;
+
+/**
+ *
+ * 给出一个类型 `T` ，以返回值真假来判断 `input` 是否为 `T` 类型
+ *
+ * @param  input 待检测的数据
+ * @param  judgingConditions 返回 boolean 来反向确认该值的类型
+ * @returns 返回 `true` 则说明该数据 `input` 类型为 `ReferenceError` ，且在 Typescript 中进行类型收缩
+ * @example
+ *
+ * ```ts
+ * import  { isType } from 'a-type-of-js';
+ *
  * console.log(isType<number>(1, true)); // true
  * console.log(isType<number>(1, () => true)); // true
  * console.log(isType<string>(1, false)); // false
  * console.log(isType<string>(1, () => false)); // false
+ *
  * console.log(isType<string>('1')); // true
  * console.log(isType<string>('1', true)); // true
  * console.log(isType<string>('1', () => true)); // true
+ *
  * console.log(isType<number>('1', false)); // false
  * console.log(isType<number>('1', () => false)); // false
+ * // 下面的例子尽然有些画蛇添足，但是在实际使用的时候却是方便的很
+ * console.log(isType<number>(1, (input) => typeof input === 'number' )); // true
+ *
+ * // 譬如 (下面的用法 （需求出自于 \@qqi/table ）)
+ *
+ * type BorderStyle = "dash" | "bold" | "double" | "simple";
+ *
+ * type UnilateralBorder = {
+ *   color: string | undefined;
+ *   style: BorderStyle
+ * }
+ *
+ * type Border = {
+ *   left: UnilateralBorder;
+ *   right: UnilateralBorder;
+ *   top: UnilateralBorder;
+ *   bottom: UnilateralBorder;
+ * }
+ *
+ *
+ * function fn (border: BorderStyle |  UnilateralBorder | Border) {
+ *    if (isType<BorderStyle>(border, input => isString(input))) {
+ *      // 然后这里 `border` 就可以当 `BorderStyle` 使用了
+ *      // 而不是每一次使用都添加上 `as BorderStyle`
+ *    } else if (isType<UnilateralBorder>(border, input => isString(input.color) || isString(input.style))) {
+ *      // 然后这里 `border` 就可以当 `UnilateralBorder` 使用了
+ *      // 而不是每一次使用都添加上 `as UnilateralBorder`
+ *      // 更关键的是上面的判断区域中不会报错，即 `input` 类型也是 `UnilateralBorder`
+ *
+ * }
+ * ...
+ *
+ *
+ * }
+ *
  * ```
  *
  */
 export function isType<T = string>(
   input: unknown,
-  judgingConditions?: boolean | (() => boolean),
+  judgingConditions: boolean | ((input: T) => boolean),
+): input is T;
+
+/**    */
+export function isType<T = string>(
+  input: unknown,
+  judgingConditions?: boolean | ((input: T) => boolean),
 ): input is T {
   if (judgingConditions === undefined) return true;
 
   if (typeof judgingConditions === 'function') {
-    return judgingConditions();
+    return judgingConditions(input as T);
   }
   return judgingConditions;
 }
